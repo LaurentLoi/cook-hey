f95cb757795fe390f58336471e17e9061fb1aad5
Laurent Loi Student <lt.loiseau+git@gmail.com>
2020-12-28 14:45:18 +0100

    Arranged routing to redirect into index.html

--- Cook-Hey/src/app/pages/index/index.component.ts
+++ Cook-Hey/src/app/pages/index/index.component.ts
@@ -1,10 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import {UserHttpService} from "../../common/common-services/http-services/user/user-http.service";
-import {AngularFirestore} from "@angular/fire/firestore";
-import {Observable} from "rxjs";
-import {Ingredient} from "../../common/models/ingredient.model";
-import {Recipe} from "../../common/models/recipe.model";
-import {IngredientsService} from "../../common/common-services/http-services/ingredients.service";
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-index',
@@ -13,26 +7,10 @@ import {IngredientsService} from "../../common/common-services/http-services/ing
 })
 export class IndexComponent implements OnInit {
 
-  users$ = this.userService.users$;
-  ingredients: Observable<Ingredient[]>;
-  filteredItem: Observable<Recipe[]>;
-
-  recipes: Observable<Recipe[]>;
-
-
   constructor(
-    private userService: UserHttpService,
-    private firestore: AngularFirestore,
-
-  ) {
-    // @ts-ignore
-    this.ingredients = firestore.collection('ingredients').valueChanges();
-    // @ts-ignore
-    this.recipes = firestore.collection('recipes').valueChanges();
-  }
+  ) {}
 
   ngOnInit(): void {
-    console.log(this.recipes);
   }
 
 }
